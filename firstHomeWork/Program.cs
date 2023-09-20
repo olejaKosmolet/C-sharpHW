b1746fd884e4411ba517011495eb8b91b5655ec1
oleja_kosmolet <olegermolaev45@gmail.com>
2023-09-20 20:21:28 +0300

    Added code

--- firstHomeWork/Program.cs
+++ firstHomeWork/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿// Task 2
+
+Console.Write("Введите 1-ое число: ");
+double firstNumber = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine($"Число {firstNumber} больше!\nЧисло {secondNumber} меньше!");
+}
+else if (firstNumber < secondNumber)
+{
+    Console.WriteLine($"Число {secondNumber} больше!\nЧисло {firstNumber} меньше!");
+}
+else if (firstNumber == secondNumber)
+{
+    Console.WriteLine($"Число {secondNumber} равно числу {firstNumber}!");
+}
