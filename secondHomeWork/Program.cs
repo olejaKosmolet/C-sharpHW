1bbf94f3cf06bddb85fa28ff0326f95081f35423
oleja_kosmolet <olegermolaev45@gmail.com>
2023-09-20 21:45:02 +0300

    Added second code

---
+++ secondHomeWork/Program.cs
@@ -0,0 +1,21 @@
+﻿// Task 2
+
+Console.Write("Введите число №1: ");
+double firstNumber = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число №2: ");
+double secondNumber = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число №3: ");
+double thirdNumber = Convert.ToDouble(Console.ReadLine());
+
+if (firstNumber > secondNumber && firstNumber > thirdNumber)
+{
+    Console.WriteLine($"Наибольшим числом является {firstNumber}");    
+}
+else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+{
+    Console.WriteLine($"Наибольшим числом является {secondNumber}");
+}
+else if (thirdNumber > firstNumber && thirdNumber > secondNumber)
+{
+    Console.WriteLine($"Наибольшим числом является {thirdNumber}");
+}
