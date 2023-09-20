6b4923ee5ff16d96c0acc380546eec9ef8987557
oleja_kosmolet <olegermolaev45@gmail.com>
2023-09-20 22:14:16 +0300

    Added fourth code

---
+++ fourthHomeWork/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task 8
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i<N; i++)
+{
+    if(i%2==0)
+    {
+    Console.Write($"{i},");
+    } 
+}
+
+Console.WriteLine($"{N}");
+
+
