159a50ff7eeae2d60a939271285c2e04815eb192
oleja_kosmolet <olegermolaev45@gmail.com>
2023-09-20 21:56:06 +0300

    Added second code

---
+++ thirdHomework/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%2 == 0)
+{
+    Console.WriteLine("Ваше число чётное!");
+}
+else
+{
+    Console.WriteLine("Ваше число нечётное!");
+}
+
+
